3881b2202b45f24b86cfb876403fb0b33e0a094a
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-10 21:07:46 +0200

    Added default styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './style/reset.scss';
+import './style/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
