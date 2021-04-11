8265d56db6550242f88468881e44fa4d226629a2
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-11 22:43:13 +0200

    Added dynamic list of expressions

--- src/components/ExpressionList.tsx
+++ src/components/ExpressionList.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Expression } from './Expression';
 import '../style/expression.scss';
 
+const initialExpressions = [
+    {
+        latex: ''
+    }
+];
+
 export const ExpressionList: React.FunctionComponent = () => {
+    const [expressions, setExpressions] = useState<Array<Expression>>(initialExpressions);
+
+    let label = 1;
+    const newExpression = {} as Expression;
     return (
         <div className='expression-list'>
-            <Expression />
-            <Expression />
-            <Expression />
-            <Expression />
+            {expressions.map((expression: Expression) => {
+                return (
+                    <Expression key={label++} label={label as unknown as string} expression={expression} expressionChange={() => {console.log('Change', expression.latex);}} expressionDelete={() => {console.log('Delete');}} expressionCreate={null} />
+                );
+            })}
+            <Expression key={label} label={label as unknown as string} expression={newExpression} expressionChange={null} expressionDelete={null} expressionCreate={() => {
+                console.log('Create');
+                newExpression.latex = '';
+                setExpressions([...expressions, newExpression]);
+            }} />
         </div>
     );
 };
