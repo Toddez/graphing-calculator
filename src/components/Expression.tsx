930df68410f698d604c13dd4f9657e9e41a94870
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-11 22:43:40 +0200

    Added expression logic

--- src/components/Expression.tsx
+++ src/components/Expression.tsx
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { addStyles, EditableMathField } from 'react-mathquill';
 import ClearIcon from '@material-ui/icons/Clear';
 
 addStyles();
 
-export const Expression: React.FunctionComponent = () => {
-    const [latex, setLatex] = useState<string>('');
+interface ExpressionProps {
+    expression: Expression,
+    label: string
+    expressionChange: ExpressionChange | null,
+    expressionDelete: ExpressionDelete | null,
+    expressionCreate: ExpressionCreate | null,
+}
 
+export const Expression: React.FunctionComponent<ExpressionProps> = ({ expression, label, expressionChange, expressionDelete, expressionCreate }) => {
     return (
-        <div className='expression'>
-            <div className='expression-label'>1</div>
-            <EditableMathField
-                className='expression-text'
-                latex={latex}
-                onChange={(mathField) => {
-                    setLatex(mathField.latex());
-                }}
-            />
-            <div className='expression-delete'><ClearIcon /></div>
+        <div className={`expression${expressionCreate ? ' expression-create' : ''}`} onClick={expressionCreate ?? undefined}>
+            <div className='expression-label'>{label}</div>
+            {expressionChange ?
+                <EditableMathField
+                    className='expression-text'
+                    latex={expression.latex}
+                    mathquillDidMount={(mathField) => {
+                        if (expression.latex === '')
+                            mathField.focus();
+                    }}
+                    onChange={(mathField) => {
+                        expression.latex = mathField.latex();
+                        if (expressionChange)
+                            expressionChange();
+                    }}
+                /> : <div className='expression-text'></div>
+            }
+            {expressionDelete ?
+                <div className='expression-delete' onClick={() => {
+                    if (expressionDelete)
+                        expressionDelete();
+                }}><ClearIcon /></div> : null
+            }
         </div>
     );
 };
