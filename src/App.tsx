fb2cbbe35853b0406b90be2b49d15dfeae6f866b
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-12 15:14:19 +0200

    Added methods to evaluate expressions

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { ExpressionList } from './components/ExpressionList';
 import { Graph } from './components/Graph';
 
-const App: React.FC = () => {
+const App: React.FunctionComponent = () => {
+    const expressionsChanged: ExpressionsChange = (expressions) => {
+        console.log('Expressions changed:', expressions);
+        evaluateExpressions(expressions);
+    };
+
+    const evaluateExpressions = (expressions: Array<Expression>) => {
+        console.log('EVAL');
+    };
+
     return (
         <div className='app'>
-            <ExpressionList />
+            <ExpressionList expressionsChange={expressionsChanged} />
             <Graph />
         </div>
     );
