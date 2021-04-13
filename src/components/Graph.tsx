150806f194b8779c663113f1d051c6220bb39b97
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-13 15:14:55 +0200

    Added props

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Canvas } from './Canvas';
 import '../style/graph.scss';
 
-export const Graph: React.FunctionComponent = () => {
+interface GraphProps {
+    expressionResults: Array<ExpressionResult>,
+    updateResolution: UpdateResolution
+}
+
+export const Graph: React.FunctionComponent<GraphProps> = ({ expressionResults, updateResolution }) => {
     return (
         <div className='graph-container'>
-            <Canvas />
+            <Canvas expressionResults={expressionResults} updateResolution={updateResolution} />
         </div>
     );
 };
