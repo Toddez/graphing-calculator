345a88c9df057b0f84206ffce1c0180037ca4c4f
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-13 15:15:29 +0200

    Updated types to match implementations

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,13 +1,34 @@
-
 type Variable = string;
+
+type Vector = Array<number>
 
 type Expression = {
     latex: string,
     code: string,
     defines: Variable | null,
-    references: Set<Variable>,
+    references: Array<Variable>,
     weight: number,
     valid: boolean
+};
+
+type Result = {
+    value: number,
+    scope: Record<string, number>
+};
+
+type ExpressionResult = {
+    expression: Expression,
+    result: Array<Result>
+};
+
+type Transform = {
+    position: Vector,
+    scale: number
+};
+
+type MouseState = {
+    down: boolean,
+    position: Vector
 };
 
 type ExpressionChange = (expression: Expression, latex: string) => void;
@@ -15,3 +36,5 @@ type ExpressionDelete = (expression: Expression) => void;
 type ExpressionCreate = () => void;
 
 type ExpressionsChange = (expressions: Array<Expression>) => void;
+
+type UpdateResolution = (transform: Transform, width: number, height: number) => void;
