2db14557aa3db5addc38881c968ce6d8b04c5d05
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-13 15:15:18 +0200

    Added re-evaluation logic and scaling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,40 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ExpressionList } from './ExpressionList';
 import { Graph } from './Graph';
 import { EvaluateExpressionWorker } from '../workers';
 import '../style/main.scss';
 
+const instance = new EvaluateExpressionWorker();
 export const Main: React.FunctionComponent = () => {
-    const expressionsChanged: ExpressionsChange = (expressions) => {
-        evaluateExpressions(expressions);
+    const [expressionResults, setExpressionResults] = useState<Array<ExpressionResult>>([]);
+    const [expressions, setExpressions] = useState<Array<Expression>>([]);
+    const [isEvaluating, setIsEvaluating] = useState<boolean>(false);
+
+    const [scope, setScope] = useState({
+        x: {
+            min: -1,
+            max: 1,
+            step:  1
+        },
+        y: {
+            min: -1,
+            max: 1,
+            step:  1
+        }
+    });
+
+    const expressionsChanged: ExpressionsChange = async (newExpressions) => {
+        if (isEvaluating)
+            return;
+
+        setIsEvaluating(true);
+        setExpressions(newExpressions);
+        setExpressionResults(await evaluateExpressions(newExpressions));
+        setIsEvaluating(false);
     };
 
-    const evaluateExpressions = async (expressions: Array<Expression>) => {
-        const instance = new EvaluateExpressionWorker();
+    const evaluateExpressions = async (expressions: Array<Expression>) : Promise<Array<ExpressionResult>> => {
         const data = {
             expressions: expressions,
-            scope: {
-                x: {
-                    min: -1920/2,
-                    max: 1920/2,
-                    step:  1
-                },
-                y: {
-                    min: -1080/2,
-                    max: 1080/2,
-                    step:  1
-                }
-            }
+            scope
         };
 
-        const processed = JSON.parse(await instance.processData(JSON.stringify(data)));
-        console.log('Results:', processed);
+        const processedData = JSON.parse(await instance.processData(JSON.stringify(data)));
+        return processedData.expressionResults;
     };
 
     return (
         <main className='main'>
             <ExpressionList expressionsChange={expressionsChanged} />
-            <Graph />
+            <Graph expressionResults={expressionResults} updateResolution={(transform, width, height) => {
+                const x = {
+                    min: (transform.position[0] - width/2) * transform.scale,
+                    max: (transform.position[0] + width/2) * transform.scale,
+                    step: transform.scale
+                };
+                const y = {
+                    min: (transform.position[1] - height/2) * transform.scale,
+                    max: (transform.position[1] + height/2) * transform.scale,
+                    step: transform.scale
+                };
+                setScope({ ...scope, x, y });
+                expressionsChanged(expressions);
+            }} />
         </main>
     );
 };
