4647afaf54eb2c045e87b9d916397668a6d29245
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-19 19:59:29 +0200

    Find discuntinuites and dynamic step size

--- src/workers/EvaluateExpressionWorker.ts
+++ src/workers/EvaluateExpressionWorker.ts
@@ -1,5 +1,6 @@
 import { evaluate } from 'mathjs';
 import memoize from 'memoizee';
+import { findDiscontinuity } from '../models/discon';
 
 const evaluateExpressions = memoize(
     (expressions: Array<string>, scope: Record<string, number>) : Array<number> => {
@@ -18,6 +19,7 @@ export const processData = (data: string) : string => {
 
     const expressions = parsedData.expressions;
     const scope = parsedData.scope;
+    const size = parsedData.size;
 
     const outData = {
         expressionResults: new Array<ExpressionResult>()
@@ -41,6 +43,8 @@ export const processData = (data: string) : string => {
         }
     }
 
+    console.time('eval');
+
     for (const variable of scopeVars) {
         const scopeVar = scope[variable];
 
@@ -56,20 +60,51 @@ export const processData = (data: string) : string => {
         }
 
         try {
+            let evalIndex = 0;
+            const originalStep = scopeVar.step;
             for (let i = scopeVar.min; i <= scopeVar.max; i += scopeVar.step) {
                 const evalScope: Record<string, number> = {};
                 evalScope[variable] = i;
 
                 const results = evaluateExpressions(evalExpressions.map((expr: Expression) => expr.code), evalScope);
-                let evalIndex = 0;
+                let exprIndex = 0;
+                let delta = 0;
                 for (let index = 0; index < expressions.length; index++)
-                    if (evalExpressions.includes(expressions[index]))
-                        outData.expressionResults[index].result = [...outData.expressionResults[index].result, { value: results[evalIndex++], scope: evalScope } ];
+                    if (evalExpressions.includes(expressions[index])) {
+                        const fn = memoize((x: number) : number => {
+                            const sc: Record<string, number> = {};
+                            Object.assign(sc, evalScope);
+                            sc.x = x;
+                            const results2 = evaluateExpressions(evalExpressions.map((expr: Expression) => expr.code), sc);
+                            return results2[exprIndex];
+                        },
+                        {
+                            primitive: true
+                        });
+
+                        const discon = findDiscontinuity(i - scopeVar.step, i, fn);
+                        if (discon)
+                            outData.expressionResults[index].expression.discontinuities.push(discon);
+
+                        const value = results[exprIndex++];
+                        if (evalIndex > 0)
+                            delta = Math.min(delta, (value - outData.expressionResults[index].result[evalIndex - 1].value));
+
+                        outData.expressionResults[index].result = [...outData.expressionResults[index].result, { value: value, scope: evalScope } ];
+                    }
+
+                scopeVar.step *= delta > originalStep ? 2 : 0.5;
+                scopeVar.step = Math.max(scopeVar.step, (scopeVar.max - scopeVar.min) / Math.max(...size));
+                evalIndex++;
             }
+
         } catch {
             console.warn('Failed to evaluate expressions');
         }
+
     }
+
+    console.timeEnd('eval');
 
     return JSON.stringify(outData);
 };
