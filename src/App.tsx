b832dbe3ac0685a535d8c3a5c453385087eab711
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-25 18:53:38 +0200

    Removed header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
 import './style/app.scss';
@@ -7,7 +6,6 @@ import './style/app.scss';
 const App: React.FunctionComponent = () => {
     return (
         <div className='app'>
-            <Header />
             <Main />
             <Footer />
         </div>
