e154917288d8810cd91e1caff6be571ac40d23f0
Teo Carlsson <carlsson.teo@gmail.com>
2022-04-24 13:23:28 +0200

    Added functions to load and save expressions in localStorage

--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,31 @@ export const newExpression = (identifier: number | (() => number) = id) => ({
   color: "#ff0000",
   discontinuities: new Array<number>(),
 });
+
+export const saveExpressions = (expressions: Array<Expression>) => {
+  try {
+    localStorage.setItem("expressions", JSON.stringify(expressions));
+  } catch {
+    console.warn("Failed to save expressions");
+  }
+};
+
+export const loadExpressions = () => {
+  try {
+    const expressions = JSON.parse(
+      localStorage.getItem("expressions") as string
+    ) as Array<Expression>;
+
+    idCounter = expressions.reduce(
+      (max, expression) => Math.max(max, expression.id + 1),
+      0
+    );
+
+    if (expressions.length === 0) expressions.push(newExpression());
+
+    return expressions;
+  } catch {
+    console.warn("Failed to load expressions");
+    return [newExpression()];
+  }
+};
